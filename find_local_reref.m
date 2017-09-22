7f2740e4207f20512f75b1c06e9a2aa2964619be
mabhijithn <mabhijithn@gmail.com>
2017-09-22 16:38:19 +0200

    Adding function to find electrode pairs from M matrix

--- find_local_reref.m
+++ find_local_reref.m
@@ -1,14 +1,14 @@
-clc;
-clear all;
-close all;
+function chnl_list = find_local_reref(M)
 %% Find channels of local re-reference
-load('nearest_neighbours.mat');
-ids = find(M==1);
-chnl_list = zeros(numel(ids),2);
-chn = 1;
-for k = 1:size(M,1)
-    ids = find(M(k,:) == 1);
-    chnl_list(chn:chn+length(ids)-1,1) = k;
-    chnl_list(chn:chn+length(ids)-1,2) = ids;
-    chn = chn+length(ids);
+% load('nearest_neighbours_8.mat');
+    ids = find(M==1);
+    chnl_list = zeros(numel(ids),2);
+    chn = 1;
+    for k = 1:size(M,1)
+        ids = find(M(k,:) == 1);
+        chnl_list(chn:chn+length(ids)-1,1) = k;
+        chnl_list(chn:chn+length(ids)-1,2) = ids;
+        chn = chn+length(ids);
+    end
+% save('nearest_neighbours_8.mat','chnl_list','-append');
 end
