6e6c8299949527ac20d8381a3b8f24da2e3348b2
mabhijithn <mabhijithn@gmail.com>
2017-09-29 13:56:08 +0200

    Modified node selection code; eliminating nodes with electrodes of already selected galvanically isolated nodes

--- do_lasso.m
+++ do_lasso.m
@@ -1,6 +1,6 @@
 function [selected_channels, lambda] = do_lasso(params, A, b, sub_name)
         load('nearest_neighbours.mat');
-        chnl_lst = chnl_list;
+       
         nofchannels = length(params.channels);
         noflags = size(A,2)/length(params.channels);
         lambda = params.lassofactor;
@@ -13,14 +13,19 @@ function [selected_channels, lambda] = do_lasso(params, A, b, sub_name)
         rem_chnl_cnt = 1;
         flag = 1;
         selected_channels = [];
+        col_sel = ones(1,size(A,2));
+        node_ids = ones(1,size(A,2)/noflags);
+        node_ids = logical(node_ids);        
         while(length(selected_channels)~=params.lassochnum || flag == 1)
-            fprintf('\nLambda = %3.2f', lambda);
-            [output,~] = group_lasso(A, b, lambda, p11, 1.0, 1.0); % AMN
+            chnl_lst = chnl_list(node_ids,:);
+            fprintf('Lambda = %3.2f of %s\n', lambda, sub_name);
+            Abar = A(:,logical(col_sel));
+            [output,~] = group_lasso(Abar, b, lambda, p11, 1.0, 1.0); % AMN
 
             flag = 0;
             decoder.ignoredchannels = find(0==mean(reshape(output,noflags,nofchannels),1)); %which channels were set to zero
             selected_channels = find(0~=mean(reshape(output,noflags,nofchannels),1)); %which channels were set to zero
-
+            fprintf('For lambda =  %3.2f no. of selected channels = %d  \n', lambda, length(selected_channels));
 
             if(length(selected_channels)>params.lassochnum)
                 lamb_gr_2 = lambda;
@@ -34,11 +39,7 @@ function [selected_channels, lambda] = do_lasso(params, A, b, sub_name)
                     lamb_ls_2 = lambda;
                     lambda = lamb_gr_2 + ((lamb_ls_2 - lamb_gr_2)/2);
                 end
-            end
-
-%             [output,~] = group_lasso(A, b, lambda, p11, 1.0, 1.0); % AMN
-%             selected_channels = find(0~=mean(reshape(output,noflags,nofchannels),1)); %which channels were set to zero
-           
+            end           
 
             if(length(selected_channels)==params.lassochnum)
                 ch_selected = chnl_lst(selected_channels,:)
@@ -46,30 +47,52 @@ function [selected_channels, lambda] = do_lasso(params, A, b, sub_name)
                 ch_repeated = ch_selected;
                 ch_repeated(uniq_chnls) = [];
                 if(length(uniq_chnls)~=(params.lassochnum*2))
-                        [r ,c] = find(ch_selected==ch_repeated(end));
-                        if(~isempty(r))
-                            row_ids = find(ismember(chnl_lst, ch_selected(r(1),:), 'rows'));
-                            strt = ((row_ids-1)*noflags)+1;
-                            stp = row_ids*noflags;
-%                             A(strt:stp,:) = [];
-                            A(:,strt:stp) = [];
-%                             b(strt:stp) = [];
 
-                            fprintf('\nRemoved channel = %d. Number of channels removed = %d', selected_channels(selected_channels == row_ids), rem_chnl_cnt);
-
-                            selected_channels(selected_channels == row_ids) = [];
-                            chnl_lst(row_ids,:) = [];
-                            nofchannels = size(chnl_lst,1);
-                            p11 = size(A,2)/nofchannels *ones(nofchannels,1); %each of the channels represents a group with length equal to number of delays
-
-                            rem_chnl_cnt = rem_chnl_cnt + 1;
-                            lamb_gr_2 = lamb_gr_2 - (0.2*lamb_gr_2);
-                            lamb_ls_2 = lamb_ls_2 + (0.2*lamb_ls_2);
-                            lambda = params.lassofactor;
+                    %% Find in channel list, the repeated electrode's channel to be removed  
+                    [r, c] = find(ch_selected == ch_repeated(end));
+                    ch_to_be_removed = ch_selected(r(end),:);
+                    row_id = find(ismember(chnl_lst,ch_to_be_removed, 'rows'));
+                    col_sel((row_id-1)*noflags+1:row_id*noflags) = 0;
+                    node_ids(row_id) = false;
+                    flag = 1; 
+                    %% Find the unique channels' electrodes' other combinations
+                    
+                    %Find unique channels first
+                    inds = [];
+                    for i = 1:length(ch_repeated)
+                        [r, c] = find(ch_selected == ch_repeated(i))
+                        inds = [inds;r]; 
+                    end
+                    inds = unique(inds);
+                    ch_selected(inds,:) = [];
+                    % Remove unique electrodes' all channels
+                    if(~isempty(ch_selected))
+                        for i = 1:numel(ch_selected)
+                            [r,c] = find(chnl_lst == ch_selected(i));
+                            for k = 1:length(r)
+                                col_sel((r(k)-1)*noflags+1:r(k)*noflags) = 0;
+                                node_ids(r(k)) = false;
+                            end
                         end
+                    % Keep unique electrode pairs channels
+                        for i = 1:size(ch_selected,1)
+                            row_id = find(ismember(chnl_lst,ch_selected(i,:), 'rows'));
+                            col_sel((row_id-1)*noflags+1:row_id*noflags) = 1;
+                            node_ids(row_id) = true;
+                        end
+                    end
+                    Abar = A(:, logical(col_sel));
+                    nofchannels = sum(col_sel == 1)/noflags;
+                    p11 = size(Abar,2)/nofchannels *ones(nofchannels,1); %each of the channels represents a group with length equal to number of delays
+% % 
+                    rem_chnl_cnt = rem_chnl_cnt + 1;
+                    lamb_gr_2 = lamb_gr_2 - (0.4*lamb_gr_2);
+                    lamb_ls_2 = lamb_ls_2 + (0.4*lamb_ls_2);
+                    lambda = params.lassofactor;
+%                         end
                 else
-                    disp('LASSO done!');
-                    save([params.resultsdir filesep sub_name sprintf('_wide_%d_selected_channels.mat',params.lassochnum)],'selected_channels','lambda');
+                    fprintf('LASSO done for %s!', sub_name);
+                    save([params.basedirectory filesep 'results/selected_channels' filesep sub_name sprintf('_wide_%d_selected_channels.mat',params.lassochnum)],'ch_selected','lambda');
                 end
             end
         end
